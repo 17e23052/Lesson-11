8e540e748fc91d4f9cd5787471cec9a75254008d
Mathman <17e23052@marling.gloucs.sch.uk>
2020-10-06 11:15:06 +0000

    GtV v2

--- main.py
+++ main.py
@@ -1,39 +1,22 @@
-#This is the 'Guess the vegetable' minigame combined with the 'Guess the animal'
-#sample script, which has been slightly modified, to make them into one program.
-print("Welcome to guess the vegetable or animal!")
-print("Please pick from ostrich, lion or whale, OR")
+#This is a 'Guess the vegetable' minigame.
+print("Welcome to guess the vegetable!")
 print("Please pick from broccoli, peas, carrot or sweetcorn!")
 print("I will attempt to guess your choice.")
 print("Please enter y or n for your answers (i.e. yes or no),")
 print("otherwise this program will not work properly.")
-print("Is your choice an animal?")
-first = input().lower()
-if first == "n":
-  print("Is it green?")
-  green = input().lower()
-  if green == "y":
-    print("Does it look like a tree?")
-    tree = input().lower()
-    if tree == "y":
-      print("It must be broccoli!")
-    elif tree == "n":
-      print("It must be peas!")
-  elif green == "n":
-    print("Is it orange?")
-    orange = input().lower()
-    if orange == "y":
-      print("It must be a carrot!")
-    elif orange == "n":
-      print("It must be sweetcorn!")
-elif first == "y":
-  print("Does the animal live in the water?")
+print("Is the vegetable green?")
+answer = input().lower()
+if answer == "y":
+  print("Does it look like a tree?")
   answer = input().lower()
-  if answer == "n":
-    print("Does the animal have wings?")
-    answer = input().lower()
-    if answer == "y":
-       print("It must be an ostrich!")
-    elif answer == "n":
-       print("It must be a lion!")
-  elif answer == "y":
-   print("It must be a whale!")
+  if answer == "y":
+    print("It must be broccoli!")
+  elif answer == "n":
+    print("It must be peas!")
+elif answer == "n":
+  print("Is it orange?")
+  answer = input().lower()
+  if answer == "y":
+    print("It must be a carrot!")
+  elif answer == "n":
+    print("It must be sweetcorn!")
