51c3f978b9352605963fe31000e0169548116ac3
Mathman <17e23052@marling.gloucs.sch.uk>
2020-10-06 11:18:41 +0000

    GtVoA v2

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
-#This is a 'Guess the vegetable' minigame.
-print("Welcome to guess the vegetable!")
+#This is the 'Guess the vegetable' minigame combined with the 'Guess the animal'
+#sample script, which has been slightly modified, to make them into one program.
+print("Welcome to guess the vegetable or animal!")
+print("Please pick from ostrich, lion or whale, OR")
 print("Please pick from broccoli, peas, carrot or sweetcorn!")
 print("I will attempt to guess your choice.")
 print("Please enter y or n for your answers (i.e. yes or no),")
 print("otherwise this program will not work properly.")
-print("Is the vegetable green?")
+print("Is your choice an animal?")
 answer = input().lower()
-if answer == "y":
-  print("Does it look like a tree?")
+if answer == "n":
+  print("Is it green?")
   answer = input().lower()
   if answer == "y":
-    print("It must be broccoli!")
+    print("Does it look like a tree?")
+    answer = input().lower()
+    if answer == "y":
+      print("It must be broccoli!")
+    elif answer == "n":
+      print("It must be peas!")
   elif answer == "n":
-    print("It must be peas!")
-elif answer == "n":
-  print("Is it orange?")
+    print("Is it orange?")
+    answer = input().lower()
+    if answer == "y":
+      print("It must be a carrot!")
+    elif answer == "n":
+      print("It must be sweetcorn!")
+elif answer == "y":
+  print("Does the animal live in the water?")
   answer = input().lower()
-  if answer == "y":
-    print("It must be a carrot!")
-  elif answer == "n":
-    print("It must be sweetcorn!")
+  if answer == "n":
+    print("Does the animal have wings?")
+    answer = input().lower()
+    if answer == "y":
+       print("It must be an ostrich!")
+    elif answer == "n":
+       print("It must be a lion!")
+  elif answer == "y":
+   print("It must be a whale!")
